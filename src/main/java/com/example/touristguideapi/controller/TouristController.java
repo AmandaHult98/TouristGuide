c4919ce9fe770d8612ff58ee0a569ed29eecbb9a
AmandaHult98 <amhu1000@stud.ek.dk>
2026-08-27 13:25:00 +0200

    Initial commit

---
+++ src/main/java/com/example/touristguideapi/controller/TouristController.java
@@ -0,0 +1,9 @@
+package com.example.touristguideapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("attractions")
+public class TouristController {
+}
---
+++ src/main/java/com/example/touristguideapi/repository/TouristRepository.java
@@ -0,0 +1,13 @@
+package com.example.touristguideapi.repository;
+
+import com.example.touristguideapi.model.TouristAttraction;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class TouristRepository {
+    private ArrayList<TouristAttraction> touristAttractions = new ArrayList<>();
+
+    
+}
