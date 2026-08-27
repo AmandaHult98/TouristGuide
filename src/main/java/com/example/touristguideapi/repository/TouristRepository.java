50f3f4f55bef786f0e840ddff608a3e3a44ac163
holmganga <siggi.sejr@gmail.com>
2026-08-27 14:58:10 +0200

    update og remove metoder tilføjet

--- src/main/java/com/example/touristguideapi/repository/TouristRepository.java
+++ src/main/java/com/example/touristguideapi/repository/TouristRepository.java
@@ -4,6 +4,7 @@ import com.example.touristguideapi.model.TouristAttraction;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /*
@@ -15,32 +16,62 @@ har set beskrivelsen af Controller klassens endpoints nedenfor.
 
 @Repository
 public class TouristRepository {
-    private ArrayList<TouristAttraction> touristAttractions = new ArrayList<>();
+    private ArrayList<TouristAttraction> touristAttractions = new ArrayList<>(
+            List.of(
+                    new TouristAttraction("Tivoli", "Forlystelsespark i indre København"),
+                    new TouristAttraction("Kronborg", "Slot i Helsingør, hvor Hamlet foregår."),
+                    new TouristAttraction("Rundetårn", "Astronomisk tårn fra Christian IVs tid.")
+            )
+    );
 
-    TouristAttraction tivoli = new TouristAttraction("Tivoli","Forlystelsespark i indre København");
-    TouristAttraction castle = new TouristAttraction("Kronborg", "Slot i Helsingør, hvor Hamlet foregår.");
-    TouristAttraction tower = new TouristAttraction("Rundetårn", "Astronomisk tårn fra Christian IVs tid.");
-
-    public void addInitialAttractions(){
-        touristAttractions.add(tivoli);
-        touristAttractions.add(castle);
-        touristAttractions.add(tower);
-    }
-
-    public void addAttraction(TouristAttraction attraction){
-        touristAttractions.add(attraction);
-    }
-
-    public ArrayList<TouristAttraction> getAllAttractions(){
+    public ArrayList<TouristAttraction> getAllAttractions() {
         return touristAttractions;
     }
 
-    public TouristAttraction findAttractionByName(String name){
-        for (TouristAttraction attraction : touristAttractions){
-            if (Objects.equals(attraction.getName(), name)){
+    public TouristAttraction findAttractionByName(String name) {
+        for (TouristAttraction attraction : touristAttractions) {
+            if (Objects.equals(attraction.getName(), name)) {
                 return attraction;
             }
         }
         return null;
     }
+
+    public void addAttraction(TouristAttraction attraction) {
+        touristAttractions.add(attraction);
+    }
+
+    public void removeAttraction(String name) {
+        for (TouristAttraction attraction : touristAttractions) {
+            if (Objects.equals(attraction.getName(), name)) {
+                touristAttractions.remove(attraction);
+            }
+            else {
+                System.out.println("No attraction with that name.");
+            }
+        }
+    }
+
+    public void updateAttractionName(String oldName, String newName) {
+        for (TouristAttraction attraction : touristAttractions){
+            if (Objects.equals(attraction.getName(), oldName)){
+                attraction.setName(newName);
+            }
+            else {
+                System.out.println("No attraction with that name.");
+            }
+        }
+    }
+
+    public void updateAttractionDescription(String name, String newDescription) {
+        for (TouristAttraction attraction : touristAttractions){
+            if (Objects.equals(attraction.getName(), name)){
+                attraction.setDescription(newDescription);
+            }
+            else {
+                System.out.println("No attraction with that name.");
+            }
+        }
+    }
+
 }
