9ab7f8548dd3ebec69e641e72061203ad1caa1ac
holmganga <siggi.sejr@gmail.com>
2026-08-27 13:50:44 +0200

    array liste af attractions oprettet med indhold og metode til at tilføje mere indhold. getter der returnere pågældende liste

--- src/main/java/com/example/touristguideapi/repository/TouristRepository.java
+++ src/main/java/com/example/touristguideapi/repository/TouristRepository.java
@@ -5,9 +5,38 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
+/*
+Tilføj en ArrayList til opbevaring af data (om ikke så længe skal I arbejde med en rigtig database).
+
+Opret et par TouristAttraction objekter, som tilføjes til denne ArrayList.
+
+Klassen skal desuden indeholde CRUD metoder, der arbejder på ovenstående ArrayList.
+
+Vent evt. med den endelige metodesignatur for CRUD metoderne til I
+har set beskrivelsen af Controller klassens endpoints nedenfor.
+*/
+
 @Repository
 public class TouristRepository {
     private ArrayList<TouristAttraction> touristAttractions = new ArrayList<>();
+
+    TouristAttraction tivoli = new TouristAttraction("Tivoli","Forlystelsespark i indre København");
+    TouristAttraction castle = new TouristAttraction("Kronborg", "Slot i Helsingør, hvor Hamlet foregår.");
+    TouristAttraction tower = new TouristAttraction("Rundetårn", "Astronomisk tårn fra Christian IVs tid.");
+
+    public void addInitialAttractions(){
+        touristAttractions.add(tivoli);
+        touristAttractions.add(castle);
+        touristAttractions.add(tower);
+    }
+
+    public void addAttraction(TouristAttraction attraction){
+        touristAttractions.add(attraction);
+    }
+
+    public ArrayList<TouristAttraction> getAllAttractions(){
+        return touristAttractions;
+    }
 
     
 }
