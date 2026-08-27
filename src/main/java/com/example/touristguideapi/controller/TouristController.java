d0e6983bdbc8c621b0deda4e5426920b437acb31
plattine <loghindan@gmail.com>
2026-08-27 13:57:22 +0200

    first commit TouristController

--- src/main/java/com/example/touristguideapi/controller/TouristController.java
+++ src/main/java/com/example/touristguideapi/controller/TouristController.java
@@ -1,9 +1,29 @@
 package com.example.touristguideapi.controller;
 
+import com.example.touristguideapi.model.TouristAttraction;
+import com.example.touristguideapi.service.TouristService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("attractions")
 public class TouristController {
+    private final TouristService service;
+
+    public TouristController(TouristService touristService) {
+        this.service = touristService;
+    }
+
+    @getMapping()
+    public ResponseEntity<ArrayList<ToursitAttraction>> getToursitAttrctions() {
+        ArrayList<TouristAttraction> attractions = service.getToursitAttractions();
+        return new ResponseEntity<>(attractions, HttpStatus.OK);
+    }
+
+
+
 }
