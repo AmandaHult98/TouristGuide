cde04a7ae08d87e17ad77ba9986284705c2858f9
AmandaHult98 <amhu1000@stud.ek.dk>
2026-09-23 11:16:02 +0200

    Udkast til test i controller

--- src/test/java/com/example/touristguideapi/controller/TouristControllerTest.java
+++ src/test/java/com/example/touristguideapi/controller/TouristControllerTest.java
@@ -107,7 +107,7 @@ class TouristControllerTest {
         TouristAttraction captured = captor.getValue();
         assertEquals("Den lille havfrue", captured.getName());
         assertEquals("Figur fra H.C.Andersens eventyr.", captured.getDescription());
-        assertEquals("København", captured.getCity());
+        //assertEquals("København", captured.getCity());
         assertEquals(EnumSet.of(Tag.ART), captured.getTags());
         //assertNotNull(captured.getOrderId()); // UUID generated as expected*/
 
